be757181f40d849e34435e1288ee61865d2f8213
Brian Morrison II <brian@brianmorrison.me>
2024-04-30 16:17:00 -0400

    profiles loading view

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -3,14 +3,17 @@ import React, { useEffect, useState } from 'react'
 import { getPublicProfiles } from '../lib/actions'
 import { UserInfo } from '../lib/models'
 import UserProfileCard from '../lib/components/UserProfileCard'
+import LoadingView from '../views/LoadingView'
 
 function Profiles() {
   const [profiles, setProfiles] = useState<UserInfo[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     async function fetchData() {
       let p = await getPublicProfiles()
       setProfiles([...p])
+      setIsLoading(false)
     }
     fetchData()
   }, [])
@@ -19,6 +22,7 @@ function Profiles() {
     <div className='flex flex-col gap-4'>
       <h1>Profiles</h1>
       <div>Members of our amazing community:</div>
+      {isLoading && <LoadingView />}
       <div className='grid grid-cols-3 gap-4'>
         {profiles.map((p, i) => <UserProfileCard key={i} userInfo={p} />)}
       </div>
