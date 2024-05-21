f11d8422828720c42249209294c4752130c255b1
Brian Morrison II <brian@brianmorrison.me>
2024-05-21 17:33:22 -0400

    cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,6 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-
-  // const a = auth();
-  // console.log(a)
-
-  // const { userId } = auth();
-  // if(userId) {
-  //   posthog.identify(userId);
-  // }
 });
 
 export const config = {
